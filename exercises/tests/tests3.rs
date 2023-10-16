41106c71ff01d260588974e3e3305a3d14494857
Natalia Pluta <pluta.natalia.m@gmail.com>
2023-10-16 09:22:14 +0200

    add resolution of tests exercises

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -19,11 +17,13 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let postive = is_even(2);
+        assert!(postive);
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let negative = is_even(3);
+        assert!(!negative);
     }
 }
